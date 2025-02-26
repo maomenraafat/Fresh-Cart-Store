44dd84342df57955961b114ee270bba61ba9f29e
Moamen Raafat <maomenraafat2000@gmail.com>
2025-02-26 11:13:41 +0200

    chore(ssr): Configure server routes for deployment - Set 'categories/:id' ,'brands/:id' to use Server-Side Rendering (SSR) - Use Prerender mode for all other routes - Ensured proper handling of dynamic and static routes for improved performance

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -14,6 +14,14 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server,
   },
   {
+    path: 'categories/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'brands/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
