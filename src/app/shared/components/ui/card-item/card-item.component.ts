95e1c11f1e488e9ce9e9c42c114758895ae23bcd
Moamen Raafat <maomenraafat2000@gmail.com>
2025-02-26 10:58:29 +0200

    fix(card): enable navigation to category and brand details
    Modified the existing card component to support navigation to category
    and brand detail pages without introducing a new component.
    Ensured smooth integration while maintaining design consistency and responsiveness.
    This change complements the previous commit  where the category-details and brand-details pages were implemented.

--- src/app/shared/components/ui/card-item/card-item.component.ts
+++ src/app/shared/components/ui/card-item/card-item.component.ts
@@ -1,12 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Category } from '../../../interfaces/category';
+import { Brand } from '../../../interfaces/brand';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-card-item',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './card-item.component.html',
   styleUrl: './card-item.component.scss',
 })
-export class CardItemComponent {
-  @Input() item!: Category;
+export class CardItemComponent implements OnInit {
+  @Input() item!: Category | Brand;
+  @Input() itemName!: string;
+
+  ngOnInit(): void {
+    console.log(this.itemName);
+  }
 }
